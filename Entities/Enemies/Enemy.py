cf5de3289509b33aff3ae991fa641a638cd878e2
Luis Henrique <luismoura2005.com>
2024-04-24 15:06:18 -0300

    FEAT: enemy moving

--- Entities/Enemies/Enemy.py
+++ Entities/Enemies/Enemy.py
@@ -1,15 +1,21 @@
-
+import pygame as pg
+from math import fabs, floor
 
 class Enemy:
-    def __init__(self, life, sprite_list, bullet_sprite, acceleration_increaser, max_velocity):
+    def __init__(self, context, life, position_dic,enemy_size, sprite_list, bullet_sprite, acceleration_increaser, max_velocity):
+        self.context = context
         self.life = life
+        self.position_dic = position_dic
+        self.enemy_size = enemy_size
         self.sprite_list = sprite_list
-        self.bullet_sprite  = bullet_sprite
+        self.bullet_sprite = bullet_sprite
         self.acceleration_increaser = acceleration_increaser
         self.max_velocity = max_velocity
 
+        self.bullets = []
+
         self.velocity = {
-            'x': 0,
+            'x': 1,
             'y': 0
         }
 
@@ -19,3 +25,70 @@ class Enemy:
         }
 
 
+        self.is_going_left = True
+        self.is_going_right = False
+    def draw(self):
+        image = pg.image.load(self.sprite_list[0])
+        image = pg.transform.scale(image, self.enemy_size)
+        self.image = self.context.screen.blit(image, (self.position_dic['x'], self.position_dic['y']))
+
+    def check_movement(self):
+
+        if self.is_in_corners():
+            if self.is_going_left:
+                self.is_going_left =  not self.is_going_left
+                self.is_going_right = not self.is_going_right
+            else:
+                self.is_going_left = not self.is_going_left
+                self.is_going_right = not self.is_going_right
+
+
+        if self.is_going_left:
+            self.move_left()
+        elif self.is_going_right:
+            self.move_right()
+        else:
+            self.stop_enemy()
+
+
+    def update(self):
+        self.velocity['x'] += self.acceleration['x']
+        self.position_dic['x'] += self.velocity['x']
+        self.check_movement()
+    def is_in_corners(self):
+        if (self.position_dic['x'] < self.enemy_size[0] ) or (self.position_dic['x'] > self.context.WINDOW_WIDTH - self.enemy_size[0] * 2):
+            return True
+        return False
+
+    def move_left(self):
+        if self.velocity['x'] > - self.max_velocity['x']:
+
+            self.acceleration['x'] = -self.acceleration_increaser
+            self.velocity['x'] += self.acceleration['x']
+
+
+    def move_right(self):
+        if self.velocity['x'] < self.max_velocity['x']:
+            self.acceleration['x'] = self.acceleration_increaser
+            self.velocity['x'] += self.acceleration['x']
+
+    def stop_enemy(self):
+
+        if floor(fabs(self.velocity['x'])) == 0:
+            pass
+        else:
+            if self.is_moving_left() :
+                self.acceleration['x'] = self.acceleration_increaser
+            elif self.is_moving_right():
+                self.acceleration['x'] = - self.acceleration_increaser
+
+
+
+    def is_moving_right(self):
+        if self.velocity['x'] > 0:
+            return True
+        return False
+    def is_moving_left(self):
+        if self.velocity['x'] < 0:
+            return True
+        return False
--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import sys
 from Entities.player import Player
 from time import sleep
 from Debug import  terminal
+from Entities.Enemies.Enemy import Enemy
 
 class Game:
     def __init__(self, win_size=(400,600)):
@@ -18,8 +19,14 @@ class Game:
         self.player = Player(self, PLAYER_INITIAL_POSITION, player_size=(60, 50) )
 
         self.screen = pg.display.set_mode(win_size)
-
+        self.time = pg.time.get_ticks()
         self.clock = pg.time.Clock()
+
+        self.enemies = [
+            Enemy(context=self,life=100, position_dic={'x': self.CENTER[0], 'y':30}, enemy_size = (60,50), \
+                  sprite_list=['Assets/basic_enemy.png'],bullet_sprite='Assets/bullet.png',\
+                  acceleration_increaser=0.2, max_velocity={'x':1.5,'y':0})
+        ]
     
     def check_events(self):
         for event in pg.event.get():
@@ -30,6 +37,13 @@ class Game:
                 if event.key == pg.K_SPACE:
                     self.player.shoot()
 
+    def draw_enemies(self):
+        for enemy in self.enemies:
+            enemy.draw()
+
+    def update_enemies(self):
+        for enemy in self.enemies:
+            enemy.update()
 
     def check_player_movements(self):
 
@@ -42,10 +56,13 @@ class Game:
             self.check_player_movements()
 
             self.player.update()
-
+            self.draw_enemies()
+            self.update_enemies()
             terminal.clear_terminal()
             print(f'Acceleration: {self.player.acceleration["x"]}')
             print(f'Velocity: {self.player.velocity["x"]}')
+            print(self.enemies[0].position_dic['x'])
+            self.time = pg.time.get_ticks()
             #sleep(.1)
 
             pg.display.flip()
